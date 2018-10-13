ab6244985f06446e6451faa28dfc1182aa4ba4d3
IvayloIV <ivvo98@abv.bg>
2018-10-13 23:18:32 +0300

    Exercise-Ropes_and_Tries

---
+++ Data-Structures-May-2017/Exercise-Ropes_and_Tries/Ropes-and-Tries/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Ropes_and_Tries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var textEditor = new TextEditor();
+            string command;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                var tokens = command.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+                DoCommand(textEditor, command, tokens);
+            }
+        }
+
+        private static void DoCommand(TextEditor textEditor, string command, string[] tokens)
+        {
+            if (tokens[0] == "login")
+            {
+                textEditor.Login(tokens[1]);
+            }
+            else if (tokens[0] == "logout")
+            {
+                textEditor.Logout(tokens[1]);
+            }
+            else if (tokens[0] == "users")
+            {
+                Console.WriteLine(String.Join(Environment.NewLine, textEditor.Users(tokens.Length == 2 ? tokens[1] : "")));
+            }
+            else if (tokens[1] == "insert")
+            {
+                var text = command.Split(new string[] { "\"" }, StringSplitOptions.None);
+                textEditor.Insert(tokens[0], int.Parse(tokens[2]), text[1]);
+            }
+            else if (tokens[1] == "prepend")
+            {
+                var text = command.Split(new string[] { "\"" }, StringSplitOptions.None);
+                textEditor.Prepend(tokens[0], text[1]);
+            }
+            else if (tokens[1] == "undo")
+            {
+                textEditor.Undo(tokens[0]);
+            }
+            else if (tokens[1] == "substring")
+            {
+                textEditor.Substring(tokens[0], int.Parse(tokens[2]), int.Parse(tokens[3]));
+            }
+            else if (tokens[1] == "clear")
+            {
+                textEditor.Clear(tokens[0]);
+            }
+            else if (tokens[1] == "delete")
+            {
+                textEditor.Delete(tokens[0], int.Parse(tokens[2]), int.Parse(tokens[3]));
+            }
+            else if (tokens[1] == "length")
+            {
+                Console.WriteLine(textEditor.Length(tokens[0]));
+            }
+            else if (tokens[1] == "print")
+            {
+                Console.WriteLine(textEditor.Print(tokens[0]));
+            }
+        }
+    }
+}
---
+++ Data-Structures-May-2017/Exercise-Ropes_and_Tries/Ropes-and-Tries/TextEditor.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using Wintellect.PowerCollections;
+
+public class TextEditor : ITextEditor
+{
+    private Trie<BigList<char>> userString;
+    private Trie<Stack<string>> userStack;
+
+    public TextEditor()
+    {
+        this.userString = new Trie<BigList<char>>();
+        this.userStack = new Trie<Stack<string>>();
+    }
+
+    public void Clear(string username)
+    {
+        if (!this.userString.Contains(username))
+        {
+            return;
+        }
+
+        UpdateHistory(username);
+        this.userString.GetValue(username).Clear();
+    }
+
+    private void UpdateHistory(string username)
+    {
+        var userStack = this.userStack.GetValue(username);
+        userStack.Push(string.Join("", this.userString.GetValue(username)));
+    }
+
+    public void Delete(string username, int startIndex, int length)
+    {
+        if (!this.userString.Contains(username))
+        {
+            return;
+        }
+
+        UpdateHistory(username);
+        this.userString.GetValue(username).RemoveRange(startIndex, length);
+    }
+
+    public void Insert(string username, int index, string str)
+    {
+        if (!this.userString.Contains(username))
+        {
+            return;
+        }
+
+        this.UpdateHistory(username);
+        this.userString.GetValue(username).InsertRange(index, str);
+    }
+
+    public int Length(string username)
+    {
+        if (!this.userString.Contains(username))
+        {
+            return 0;
+        }
+
+        return this.userString.GetValue(username).Count;
+    }
+
+    public void Login(string username)
+    {
+        this.userString.Insert(username, new BigList<char>());
+        this.userStack.Insert(username, new Stack<string>());
+    }
+
+    public void Logout(string username)
+    {
+        if (!this.userString.Contains(username))
+        {
+            return;
+        }
+        this.userString.Delete(username);
+        this.userStack.Delete(username);
+    }
+
+    public void Prepend(string username, string str)
+    {
+        if (!this.userString.Contains(username))
+        {
+            return;
+        }
+
+        this.UpdateHistory(username);
+        this.userString.GetValue(username).AddRangeToFront(str);
+    }
+
+    public string Print(string username)
+    {
+        if (!this.userString.Contains(username))
+        {
+            return "";
+        }
+        return String.Join("", this.userString.GetValue(username));
+    }
+
+    public void Substring(string username, int startIndex, int length)
+    {
+        if (!this.userString.Contains(username))
+        {
+            return;
+        }
+
+        this.UpdateHistory(username);
+        var userString = this.userString.GetValue(username);
+        var elements = userString.GetRange(startIndex, length);
+        this.userString.Insert(username, new BigList<char>(elements));
+    }
+
+    public void Undo(string username)
+    {
+        if (!this.userString.Contains(username))
+        {
+            return;
+        }
+
+        var userStack = this.userStack.GetValue(username);
+        if (userStack.Count == 0)
+        {
+            return;
+        }
+        var lastUserString = userStack.Pop();
+        this.UpdateHistory(username);
+        this.userString.Insert(username, new BigList<char>(lastUserString));
+    }
+
+    public IEnumerable<string> Users(string prefix = "")
+    {
+        foreach (var user in userString.GetByPrefix(prefix))
+        {
+            yield return user;
+        }
+    }
+}
